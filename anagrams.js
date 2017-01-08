cf508113ec2ad0b93977b2ae6d2ae4bd808a06ce
Alex W <wapniarski@gmail.com>
2017-01-07 19:02:16 -0800

    first commit

---
+++ anagrams.js
@@ -0,0 +1,33 @@
+function anagrams(arr){
+  var word; // used to store word before inserting into object
+  var anagramCount = {} // used as return object
+  
+  // sort letters of each word in array
+  for(var i = 0; i < arr.length; i++){
+    
+    // sort letters: split word, sort array of letters, join back into word
+    word = arr[i].split("").sort().join("");
+    
+    // if word doesn't exist, create key and count one
+    if(!anagramCount[word]){
+      anagramCount[word] = 1;
+    }
+    
+    // if word exists, increment count 
+    else{
+      anagramCount[word] += 1;
+    }
+  }
+  
+  //return anagramCount
+  return anagramCount;
+}
+
+var test1 = anagrams(['abc','bca','dog','god','cat','act','pop'])
+console.log(test1);
+
+var test2 = anagrams(['car','rac','awesome','sauce'])
+console.log(test2);
+
+var test3 = anagrams(['look','kool','book','koob','a'])
+console.log(test3);
